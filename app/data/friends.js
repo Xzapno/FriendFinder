0c9872d9d53cfadef26b8d80bc11aa571e1b20d4
Paul <hoffsbox@gmail.com>
2018-07-24 17:02:56 -0400

    Monday Update

---
+++ app/data/friends.js
@@ -0,0 +1,125 @@
+var friends = [
+
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"LLoyd",
+        "photo":"http://images.entertainment.ie/images_content/rectangle/620x372/dumb.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Harry",
+        "photo":"http://bellobitesback.files.wordpress.com/2012/03/dumb-and-dumber-toilet-scene2.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Joe",
+        "photo":"http://www.postavy.cz/foto/joe-mentalino-foto.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Mary",
+        "photo":"https://www.aveleyman.com/Gallery/2017/H/8134-5456.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Sea Bass",
+        "photo":"https://www.aveleyman.com/Gallery/2017/N/64837-5456.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"JP",
+        "photo":"https://www.aveleyman.com/Gallery/2017/D/4967-5456.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+
+
+]
+
+  
---
+++ server.js
@@ -0,0 +1,40 @@
+// Setup of global variables & Dependancies
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// EXPRESS CONFIGURATION
+// Setup express server
+
+var app = express();
+
+// Setup port for listner
+
+var PORT = process.env.PORT || 8080;
+
+// Setup the Express app to handle data parsing
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// LISTENER
+// Starts listening on our server
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
+
+
+
+
+
+
